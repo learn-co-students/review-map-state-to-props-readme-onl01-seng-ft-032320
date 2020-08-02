6f7268a08f49ea54f3a128e2d08f967f1c7cb36e
Diane Crame <dcrame@icloud.com>
2020-08-01 17:19:39 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import './App.css';
 class App extends Component {
 
   handleOnClickItems() {
-    this.props.store.dispatch({
+    this.props.dispatch({
       type: 'GET_COUNT_OF_ITEMS',
     });
   }
 
   handleOnClickUsers() {
-    this.props.store.dispatch({
+    this.props.dispatch({
       type: 'GET_COUNT_OF_USERS',
     });
   }
@@ -21,20 +21,20 @@ class App extends Component {
     // debugger;
     return (
       <div className="App">
-          <button onClick={() => this.handleOnClickItems()}>
-            Click to change items count
+        <button onClick={() => this.handleOnClickItems()}>
+          Click to change items count
             </button>
-          <button onClick={() => this.handleOnClickUsers()}>
-            Click to change user count
+        <button onClick={() => this.handleOnClickUsers()}>
+          Click to change user count
           </button>
-          <p>{this.props.items.length}</p>
+        <p>{this.props.items.length}</p>
       </div>
     );
   }
 }
 
 const mapStateToProps = (state) => {
-  debugger;
+  // debugger;
   return { items: state.items }
 }
 
